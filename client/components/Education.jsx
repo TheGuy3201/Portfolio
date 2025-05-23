40380173c2e343365ddbf18c0829d9a3e46f5e5a
Joshua Desroches <josh.des.21@gmail.com>
2025-05-23 12:14:03 -0400

    23/05/25 - Completed entire website, Projects completed, comments added to each component

--- client/components/Education.jsx
+++ client/components/Education.jsx
@@ -1,8 +1,12 @@
 export default function Education() {
     return (
     <>
-        <h2>Education</h2>
+        {/* Education page, its relatively simple at the moment due to my lack of extensive education*/}
+        <h1>Education</h1>
+        {/* Contains information about my educational background */}
         <div className="EducationPanel">
+            
+            {/* Each education entry is split into cards to better organize the information */}
             <div className="EducationCard">
                 <h2>Centennial College</h2>
                 <p>Game - Programming Advanced Diploma</p>
@@ -10,6 +14,7 @@ export default function Education() {
                 <p>GPA: 4.0/4.5</p>
                 <img className = "SelfImg" src="https://oaa.on.ca/Assets/Common/Shared_Images/Awards/2024/1200x675%20DE-03.png" alt="Centennial College" />
 
+                {/* A list of important courses I took in said program */}
                 <ul className="CourseList">
                     <p>Relevant Courses:</p>
                     <li>Java Programming</li>
@@ -20,12 +25,13 @@ export default function Education() {
                     <li>Software Testing & Quality</li>
                 </ul>
             </div>
+
+            {/* A relatively simple high school education card */}
             <div className="EducationCard">
                 <h2>High School</h2>
                 <p>Sir Oliver Mowat Collegiate</p>
                 <p>Graduated: June 2023</p>
                 <p>Honour Roll Grades: 9, 10, 11, 12</p>
-                
             </div>
         </div>
     </>
--- client/components/Services.jsx
+++ client/components/Services.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// Services component that displays a list of services offered
 const services = [
   {
     title: "Front-End Web Development",
@@ -18,7 +19,7 @@ const services = [
   },
   {
     title: "Back-End Development",
-    description: "With the use of Node.js, Express, and MongoDB. I build robust server-side applications and APIs such as this one!.",
+    description: "With the use of Node.js, Express, and MongoDB. I build robust server-side applications and APIs such as this one!",
     image: "https://img.freepik.com/premium-vector/backend-development-icon-vector-image-can-be-used-computer-programming_120816-101610.jpg"
   },
   {
@@ -31,6 +32,7 @@ const services = [
 export default function Services() {
   const [flipped, setFlipped] = useState(Array(services.length).fill(false));
 
+  // Function to toggle the flipped state of a service card
   const toggleFlip = (index) => {
     setFlipped((prev) => {
       const updated = [...prev];
@@ -41,13 +43,17 @@ export default function Services() {
 
   return (
     <>
-      <h2>Services</h2>
+      {/* Services page */}
+      <h1>Services</h1>
+
+      {/* Services Panel */}
       <div className="ServicesPanel">
         {services.map((service, index) => (
           <div
             className={`ServiceCard ${flipped[index] ? "flipped" : ""}`}
             key={index}
           >
+            {/* Front of service card*/}
             <div className="ServiceCardInner">
               <div
                 className="ServiceCardFront"
@@ -56,6 +62,8 @@ export default function Services() {
                 <img src={service.image} alt={service.title} />
                 <h2>{service.title}</h2>
               </div>
+              
+              {/* Back of service card */}
               <div className="ServiceCardBack">
                 <h2>{service.title}</h2>
                 <p>{service.description}</p>
